fa9487fec106a50b2165002f211fd78ca91800f2
unknown <nkosimova06@gmail.com>
2020-05-01 18:40:37 +0500

    try to use Parallel class

--- Class Waterfall.cs
+++ Class Waterfall.cs
@@ -1,18 +1,44 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 namespace HW30
 {
+    public  class Point
+    {
+        public  int col{get; set;}
+        public  int row{get; set;}
+    }
     public static class WaterFall
     {
         public static int row;
         public static int column;
+        public static int height;
 
-        public static void WriteAt(string s, object obx, object  oby)
+        public static void WriteAt(object p)
         {
-            int x = (int)obx;
-            int y = (int)oby;
+            string s = "h";
+            Point point = p as Point;
 
-            Console.SetCursorPosition(column + x, row + y);
+            Console.SetCursorPosition(column + point.col, row + point.row);
             Console.Write(s);
+            Thread.Sleep(500);
+            Console.Clear();
+        }
+        public static void Stream(int column)
+        {
+           // WaterFall.row = Console.CursorTop;
+           // WaterFall.column = Console.CursorLeft;
+            Parallel.For(1,column, (j)=>{
+                Point point = new Point();
+                point.col = j;
+                for (int i = 0; i <= height; i++)
+                {
+                    point.row = i;
+                    WaterFall.WriteAt(point);
+                }
+            });
+            Console.ReadLine();
+            Console.Clear();
         }
         
     }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace HW30._04
 {
@@ -8,14 +9,31 @@ namespace HW30._04
         static void Main(string[] args)
         {
             Console.Clear();
+            WaterFall.height = 5;
             WaterFall.row = Console.CursorTop;
             WaterFall.column = Console.CursorLeft;
-            TimerCallback tm = new TimerCallback((i)=>WaterFall.WriteAt("h",(int)i,(int)i));
-            for (var i = 0; i <= 10; i++)
-            {
-                Timer timer = new Timer(tm,i,0,2000);
+            int num = 5;
+            Point point = new Point();
+                point.col = num;
+                for (int i = 0; i <= 5; i++)
+                {
+                    point.row = i;
+                    WaterFall.WriteAt(point);
+                }
+           // WaterFall.Stream(num);
+          //  TimerCallback tm = new TimerCallback(WaterFall.Stream);
+            
+          /*  Task task1 = Task.Factory.StartNew((i) => {
+                
+                Timer timer = new Timer(tm, i, 0, 8000);
+            },5);
 
-            }
+            Task task2 = Task.Factory.StartNew((i) => {
+             //   TimerCallback tm = new TimerCallback(WaterFall.Stream);
+                Timer timer = new Timer(tm, i, 0, 8000);
+            },2);*/
+            //task1.Start();
+          //  Console.ReadLine();
         }
     }
 }
